b3a30e8915815e862d2672b8caea64b109924905
Balakrishnan2k17 <48944261+Balakrishnan2k17@users.noreply.github.com>
2024-07-07 18:39:20 +0530

    Update provider.tf

--- s3ot/provider.tf
+++ s3ot/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-west-2"
-  access_key = "AKIAST4BTQET2NURJV5B"
-  secret_key = "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"
+  //access_key = "AKIAST4BTQET2NURJV5B"
+  //secret_key = "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"
 }
